d96634598ce286438b2e3bc7a3789de7b2c2f5d2
Akash Mohan <akhikm4@gmail.com>
2025-05-17 21:14:05 -0500

    idk commit lol

--- backend/src/main/java/com/swipebyte/project/repository/UserRepository.java
+++ backend/src/main/java/com/swipebyte/project/repository/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.swipebyte.project.entity.UserEntity;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {

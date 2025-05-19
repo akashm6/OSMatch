46020a291484d9db58bf5fb669dca6cf8e90f397
Akash Mohan <akhikm4@gmail.com>
2025-05-19 15:05:27 -0500

    added logic to fetch likedProjects on profile

---
+++ backend/src/main/java/com/swipebyte/project/controllers/ProfileController.java
@@ -0,0 +1,64 @@
+package com.swipebyte.project.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import java.util.*;
+import com.swipebyte.project.dto.*;
+import com.swipebyte.project.entity.*;
+import com.swipebyte.project.repository.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class ProfileController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private ProfileRepository profileRepo;
+
+    @Autowired
+    private ProjectRepository projectRepo;
+
+    // Grabs the user's swiped right projects from db to display on profile
+    @GetMapping("/profile/likedProjects")
+    public ResponseEntity<?> getLikedProjects(@RequestParam Long userId) {
+
+        UserEntity user = userRepo.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "user not found from profile"));
+
+        List<Project> projects = projectRepo.findByUser(userId);
+
+        return ResponseEntity.ok(projects);
+    }
+
+    @PostMapping("/profile/edit")
+    public ResponseEntity<?> editProfile(@RequestBody ProfileDto profile) {
+
+        UserEntity user = userRepo.findById(profile.getUserId()).get();
+
+        UserProfile currProfile = user.getProfile();
+
+        if (currProfile == null) {
+
+            return ResponseEntity.badRequest().body("Profile not found.");
+        }
+        String currentBio = currProfile.getBio();
+        String newBio = profile.getBio();
+        if (!newBio.equals(currentBio)) {
+
+            currProfile.setBio(newBio);
+        }
+
+        profileRepo.save(currProfile);
+
+        return ResponseEntity.ok("Profile changed!");
+    }
+
+}

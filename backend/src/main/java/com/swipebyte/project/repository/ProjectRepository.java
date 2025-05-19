7690be1c8fcaececa3037be2bf0057a141de2913
Akash Mohan <akhikm4@gmail.com>
2025-05-19 15:07:10 -0500

    logic to grab all liked projects by userId

--- backend/src/main/java/com/swipebyte/project/repository/ProjectRepository.java
+++ backend/src/main/java/com/swipebyte/project/repository/ProjectRepository.java
@@ -2,13 +2,21 @@ package com.swipebyte.project.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.swipebyte.project.entity.Project;
+import com.swipebyte.project.entity.UserEntity;
+
+import lombok.NonNull;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
     List<Project> findByUrl(String url);
+
+    @Query("SELECT p from Project p WHERE p.user.id = :userId")
+    List<Project> findByUser(@Param("userId") Long userId);
 
     boolean existsByUrl(String url);
 }

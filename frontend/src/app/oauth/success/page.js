7c65abe4ff56afabe45ed211cfd11de86006447d
Akash Mohan <akhikm4@gmail.com>
2025-05-22 23:26:21 -0500

    page for oauth success token storage and redirect

---
+++ frontend/src/app/oauth/success/page.js
@@ -0,0 +1,28 @@
+'use client'
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+
+export default function OAuthSuccessPage() {
+    const router = useRouter();
+    const params = useSearchParams();
+
+    useEffect(() => {
+
+        const token = params.get("token");
+        const userId = params.get("userId");
+        if(token) {
+            localStorage.setItem("token", token);
+            localStorage.setItem("userId", userId);
+            router.push("/home");
+        }
+    }, []);
+
+    console.log(params);
+
+    return <p>Logging in...</p>;
+    
+    
+}

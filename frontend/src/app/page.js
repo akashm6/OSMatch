0b935600c7c3d3b609c37dc97283c86bf46b32dc
Akash Mohan <akhikm4@gmail.com>
2025-05-25 15:48:14 -0500

    cleaned up landing page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,9 +2,45 @@
 import { useRouter } from "next/navigation";
 import { LoginForm } from "./components/login-form";
 
+import { useState } from "react";
+
 export default function LandingPage() {
   const router = useRouter();
 
+  const [formData, setFormData] = useState({
+          email: "",
+          password: "",
+      });
+  const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+        const response = await fetch("http://localhost:8080/auth/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(formData),
+        });
+        const data = await response.json(); 
+        console.log(data)
+        if (response.ok) {
+            localStorage.setItem("token", data.token);
+            localStorage.setItem("userId", data.userId);
+            localStorage.setItem('bio', data.bio);
+            setMessage(data.message || "Login successful!");
+            router.push("/home"); 
+        } else {
+            setMessage(data.message || "Invalid credentials.");
+        }
+    } catch (error) {
+        console.error("Error:", error);
+        setMessage("An error occurred. Please try again.");
+    }
+};
+  
   return (
     <div style={{ textAlign: "center", padding: "2rem" }}>
       <h1>Welcome to Project Tinder</h1>

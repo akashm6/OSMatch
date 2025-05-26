c9031b2945c0c51c6080b7820bf2b09235164ffb
Akash Mohan <akhikm4@gmail.com>
2025-05-26 14:58:53 -0500

    mail sender bean

--- backend/src/main/java/com/osmatch/project/securityconfig/SecurityConfig.java
+++ backend/src/main/java/com/osmatch/project/securityconfig/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.osmatch.project.securityconfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -50,4 +51,5 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }

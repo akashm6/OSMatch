68fca7323f894f6b70e6067d171c1290a3465011
Akash Mohan <akhikm4@gmail.com>
2025-05-26 16:18:44 -0500

    added full logic for changing a password

--- backend/src/main/java/com/osmatch/project/controllers/PasswordChangeController.java
+++ backend/src/main/java/com/osmatch/project/controllers/PasswordChangeController.java
@@ -1,7 +1,9 @@
 package com.osmatch.project.controllers;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Map;
+import java.util.UUID;
 
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,10 +17,13 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.osmatch.project.dto.PassChangeDto;
+import com.osmatch.project.entity.PasswordResetToken;
 import com.osmatch.project.entity.UserEntity;
+import com.osmatch.project.repository.PasswordResetTokenRepository;
 import com.osmatch.project.repository.UserRepository;
 
 @RestController
@@ -26,6 +31,9 @@ public class PasswordChangeController {
 
     @Autowired
     UserRepository userRepo;
+
+    @Autowired
+    PasswordResetTokenRepository passRepo;
 
     @Autowired
     JavaMailSender mailSender;
@@ -36,6 +44,7 @@ public class PasswordChangeController {
     @PostMapping("/auth/sendEmail")
     public ResponseEntity<?> sendPasswordEmail(@RequestBody String email) {
         SimpleMailMessage message = new SimpleMailMessage();
+
         message.setTo(email);
         message.setSubject("Reset Password - OSMatch");
         String redirecturl = "http://localhost:3000/auth/passwordChange";
@@ -43,26 +52,71 @@ public class PasswordChangeController {
                 + "\n\nThis link will expire in 15 minutes.";
 
         message.setText(text);
+
         int expirationTime = 900000;
-        Date now = new Date();
         Date expiration = new Date(System.currentTimeMillis() + expirationTime);
-        System.out.print("THIS IS THE EXPIRATION TIME: " + expiration);
+        String token = UUID.randomUUID().toString();
+        UserEntity user = userRepo.findByEmail(email);
+
+        PasswordResetToken passResetToken = new PasswordResetToken();
+        passResetToken.setEmail(email);
+        passResetToken.setUser(user);
+        passResetToken.setExpiryDate(expiration);
+        passResetToken.setToken(token);
+        passResetToken.setUsed(false);
+        passRepo.save(passResetToken);
+
         mailSender.send(message);
+
         return ResponseEntity.ok(Map.of(
                 "message", "A link to reset your password has been sent to your email.",
                 "expiration", expiration));
     }
 
     @PostMapping("/auth/passwordChange")
-    public ResponseEntity<?> changePassword(@RequestBody PassChangeDto PassDto) {
-        String newPass = PassDto.getNewPassword();
-        UserEntity user = userRepo.findById(PassDto.getUserId())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found."));
-        if (user == null) {
-            return ResponseEntity.badRequest().body("User cannot be found with userId.");
+    public ResponseEntity<?> changePassword(@RequestParam String token, @RequestBody PassChangeDto PassDto) {
+
+        PasswordResetToken passResetToken = passRepo.findByToken(token)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid token."));
+        if (passResetToken == null) {
+            return ResponseEntity.badRequest().body(Map.of(
+                    "message", "Invalid token. Redirect."));
         }
 
-        return null;
-    }
+        if (passResetToken.getUsed() == true) {
+            return ResponseEntity.badRequest().body(Map.of(
+                    "message", "Token already used. Redirect."));
+        }
 
+        Date expiryDate = passResetToken.getExpiryDate();
+        Date now = new Date();
+
+        if (now.compareTo(expiryDate) > 0) {
+            return ResponseEntity.badRequest().body(Map.of(
+                    "message", "Link Expired. Redirect."));
+        }
+
+        String newPass = PassDto.getNewPassword();
+        String confirmedPass = PassDto.getConfirmedPassword();
+
+        if (newPass.compareTo(confirmedPass) != 0) {
+            return ResponseEntity.badRequest().body(Map.of(
+                    "message", "Passwords do not match. Try again."));
+        }
+
+        UserEntity user = passResetToken.getUser();
+        if (user == null) {
+            return ResponseEntity.badRequest().body(Map.of(
+                    "message", "User cannot be found with this token. Redirect."));
+        }
+
+        user.setPassword(encoder.encode(newPass));
+        userRepo.save(user);
+
+        passResetToken.setUsed(true);
+        passRepo.save(passResetToken);
+
+        return ResponseEntity.ok(Map.of(
+                "message", "Password successfully changed. Redirecting to login."));
+    }
 }

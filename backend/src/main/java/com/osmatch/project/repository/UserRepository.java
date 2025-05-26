30bc1209a90c7d809bf38890fa4a5c6a8934fb1e
Akash Mohan <akhikm4@gmail.com>
2025-05-26 16:19:35 -0500

    updatePassword function in userRepo

--- backend/src/main/java/com/osmatch/project/repository/UserRepository.java
+++ backend/src/main/java/com/osmatch/project/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.osmatch.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.osmatch.project.entity.UserEntity;
@@ -12,5 +15,9 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE UserEntity u SET u.password = :newPass WHERE u.password = :oldPass")
+    void updatePassword(@Param("oldPass") String oldPass, @Param("newPass") String newPass);
 
 }

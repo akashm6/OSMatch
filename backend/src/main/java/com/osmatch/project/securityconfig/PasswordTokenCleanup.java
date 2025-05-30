085e634fb73313118420a042197f989113f87a49
Akash Mohan <akhikm4@gmail.com>
2025-05-30 14:35:07 -0500

    service for garbage collection on expired password reset tokens

---
+++ backend/src/main/java/com/osmatch/project/securityconfig/PasswordTokenCleanup.java
@@ -0,0 +1,25 @@
+package com.osmatch.project.securityconfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import java.util.Date;
+
+import com.osmatch.project.repository.PasswordResetTokenRepository;
+
+@Service
+public class PasswordTokenCleanup {
+
+    @Autowired
+    PasswordResetTokenRepository tokenRepo;
+
+    // schedule a service to remove expired or invalid tokens
+    @Scheduled(fixedRate = 3600000)
+    public void removeExpiredTokens() {
+
+        Date now = new Date();
+        tokenRepo.deleteExpiredTokens(now);
+
+    }
+
+}

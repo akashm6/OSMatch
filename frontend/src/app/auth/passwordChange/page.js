e4eff587c703d62fffe6c4eafda2691a261c7cb6
Akash Mohan <akhikm4@gmail.com>
2025-05-30 15:02:37 -0500

    added new page for email redirect

--- frontend/src/app/auth/passwordChange/page.js
+++ frontend/src/app/auth/passwordChange/page.js
@@ -3,19 +3,23 @@ import {useState, React} from "react";
 import { useSearchParams } from "next/navigation";
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
  
 
 export default function passwordChangePage() {
 
+    const router = useRouter();
     const params = useSearchParams();
     const token = params.get("token");
-    console.log("this is the token: ", token);
+
     const [formData, setFormData] = useState(
         {
             "newPassword": '',
             "confirmedPassword": '',
         }
     )
+
+    const [response, setResponse] = useState('');
     
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -23,7 +27,6 @@ export default function passwordChangePage() {
           ...prev,
           [name]: value  
         }));
-        console.log(formData);
     }
     
     const handleSubmit = async (e) => {
@@ -35,45 +38,46 @@ export default function passwordChangePage() {
                 body: JSON.stringify(formData)
             });
             const data = await res.json();
-            console.log(data.message);
+            setResponse(data.message || "");
+            if(data.redirect) {
+                router.push("/")
+            }
         }
-        
         catch(error) {
             console.error(error);
-        }
-
+        }    
     }
-        
+
     return (
-    <div>
-    <form onSubmit={handleSubmit}>
         <div>
-            <Input 
-            id="newPassword"
-            name = "newPassword"
-            value = {formData.newPassword}
-            type ="password"
-            placeholder="New Password"
-            onChange = {handleChange} 
-            required
-            />
+        <form onSubmit={handleSubmit}>
+            <div>
+                <Input 
+                id="newPassword"
+                name = "newPassword"
+                value = {formData.newPassword}
+                type ="password"
+                placeholder="New Password"
+                onChange = {handleChange} 
+                required
+                />
 
-            <Input 
-            id="confirmedPassword"
-            name = "confirmedPassword" 
-            value = {formData.confirmedPassword}
-            type="password" 
-            placeholder="Confirm Password" 
-            onChange = {handleChange}
-            required
-            />
-            <Button type="submit" className="w-full">
-                Change Password.
-            </Button>
+                <Input 
+                id="confirmedPassword"
+                name = "confirmedPassword" 
+                value = {formData.confirmedPassword}
+                type="password" 
+                placeholder="Confirm Password" 
+                onChange = {handleChange}
+                required
+                />
+                <Button type="submit" className="w-full">
+                    Change Password.
+                </Button>
+            </div>
+        </form>
+
+        {response && <p>{response}</p>}
         </div>
-    </form>
-    </div>
-        )
-
-
+    )
 }

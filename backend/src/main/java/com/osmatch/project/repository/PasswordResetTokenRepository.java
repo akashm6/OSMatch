b0d30999b06e4a5852479193684439212385622a
Akash Mohan <akhikm4@gmail.com>
2025-05-30 14:34:43 -0500

    added query for removing expired/used tokens

--- backend/src/main/java/com/osmatch/project/repository/PasswordResetTokenRepository.java
+++ backend/src/main/java/com/osmatch/project/repository/PasswordResetTokenRepository.java
@@ -1,14 +1,20 @@
 package com.osmatch.project.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.osmatch.project.entity.PasswordResetToken;
+import java.util.Date;
 import java.util.Optional;
 
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, String> {
 
     Optional<PasswordResetToken> findByToken(String token);
+
+    @Modifying
+    @Query("DELETE FROM PasswordResetToken t WHERE t.expiryDate <= :now OR t.used = true")
+    void deleteExpiredTokens(@Param("now") Date now);
 
 }

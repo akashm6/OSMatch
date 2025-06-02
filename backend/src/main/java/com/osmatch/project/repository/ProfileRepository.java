b22367607cb0048f9b2553b981a1e100b3aed420
Akash Mohan <akhikm4@gmail.com>
2025-06-02 16:38:47 -0500

    removed redundancies

--- backend/src/main/java/com/osmatch/project/repository/ProfileRepository.java
+++ backend/src/main/java/com/osmatch/project/repository/ProfileRepository.java
@@ -1,14 +1,9 @@
 package com.osmatch.project.repository;
 
-import java.util.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.osmatch.project.entity.*;
-
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.lang.NonNull;
 
 @Repository
 public interface ProfileRepository extends JpaRepository<UserProfile, Long> {

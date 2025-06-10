280b70219ab2eca1ab174edeae21fba5aa6846a6
Akash <akhikm4@gmail.com>
2025-06-09 22:13:18 -0500

    cleanup

--- backend/src/main/java/com/osmatch/project/repository/ProfileRepository.java
+++ backend/src/main/java/com/osmatch/project/repository/ProfileRepository.java
@@ -8,6 +8,5 @@ import com.osmatch.project.entity.*;
 @Repository
 public interface ProfileRepository extends JpaRepository<UserProfile, Long> {
 
-    // @Query("SELECT u FROM UserProfile u WHERE u.user.id = :userId")
     UserProfile findByUser_Id(Long userId);
 }
